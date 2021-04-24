d60a40c18c8042de9b01ea4b434d1b9b69f34e1a
Nason Drake <39218689+MaskedRiderDecade@users.noreply.github.com>
2021-04-24 19:01:06 +0800

    初始化项目

---
+++ src/main/java/com/jxw/icharity/IcharityApplication.java
@@ -0,0 +1,13 @@
+package com.jxw.icharity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcharityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcharityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jxw/icharity/IcharityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxw.icharity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcharityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
